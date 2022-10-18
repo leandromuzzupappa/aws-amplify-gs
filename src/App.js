b5373d0db84d6cd68257f6a5a6536d037bb07e55
LMuzzupappa <leandro.muzzupappa@mediamonks.com>
2022-10-18 01:14:58 -0300

    basic crud

--- src/App.js
+++ src/App.js
@@ -1,24 +1,70 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+
+import { useState, useEffect } from "react";
+import { Amplify, API, graphqlOperation } from "aws-amplify";
+import { createTask } from "./graphql/mutations";
+import { listTasks } from "./graphql/queries";
 
 function App() {
+  const [task, setTask] = useState({
+    name: "",
+    description: "",
+  });
+  const [tasks, setTasks] = useState([]);
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    console.log(task);
+
+    const result = await API.graphql(
+      graphqlOperation(createTask, { input: task })
+    );
+
+    console.log(result);
+  };
+
+  useEffect(() => {
+    async function loadTasks() {
+      const result = await API.graphql(graphqlOperation(listTasks));
+      setTasks(result.data.listTasks.items);
+      console.log(result);
+    }
+
+    loadTasks();
+
+    return () => {};
+  }, []);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <form onSubmit={handleSubmit}>
+        <input
+          type="text"
+          name="name"
+          placeholder="Title"
+          onChange={(e) => setTask({ ...task, name: e.target.value })}
+        />
+        <textarea
+          name="description"
+          id="description"
+          cols="30"
+          rows="10"
+          onChange={(e) => setTask({ ...task, description: e.target.value })}
+        ></textarea>
+
+        <button>Submit</button>
+      </form>
+
+      {/* List tasks using aws ui library */}
+      <ul>
+        {tasks.map((task) => (
+          <li key={task.id}>
+            <h3>{task.name}</h3>
+            <p>{task.description}</p>
+          </li>
+        ))}
+      </ul>
+    </>
   );
 }
 
